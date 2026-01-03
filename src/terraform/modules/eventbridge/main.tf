251eb1d853ca1085737eea9a8d3bc0add5cfe15a
Leonard Jones <leojones4902@gmail.com>
2026-01-02 23:43:24 -0500

    update runpod connection name

--- src/terraform/modules/eventbridge/main.tf
+++ src/terraform/modules/eventbridge/main.tf
@@ -17,7 +17,7 @@ module "runpod_connection" {
   create_api_destinations = false
 
   connections = {
-    runpod = {
+    "${var.project_name}_${var.environment}_upscaler_runpod" = {
       authorization_type = "API_KEY"
       auth_parameters = {
         api_key = {
